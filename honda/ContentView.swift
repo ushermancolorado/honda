a0b4f24b8a6d18edf6f3019f5f9b989d34349546
usherman3741 <usherman3741@gmail.com>
2024-11-01 16:23:13 +0900

    test commit

--- honda/ContentView.swift
+++ honda/ContentView.swift
@@ -10,13 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "2.circle")
+                .resizable()
                 .imageScale(.large)
+                .scaledToFit()
                 .foregroundStyle(.tint)
+            
+            DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+            
             Text("Hello, world!")
+                .fontWeight(.black).font(.callout)
+                .padding(40)
+                .fontWidth(.compressed)
+                .fontWeight(.bold)
+                .font(.largeTitle.bold().bold())
+            
+            Gauge(value: 0.1, in: /*@START_MENU_TOKEN@*/0...1/*@END_MENU_TOKEN@*/) {
+                Text("Labedddl")
+            }
+            
         }
         .padding()
+        .foregroundColor(.red)
     }
+    
 }
 
 #Preview {
--- honda/hondaApp.swift
+++ honda/hondaApp.swift
@@ -12,6 +12,7 @@ struct hondaApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+
         }
     }
 }
