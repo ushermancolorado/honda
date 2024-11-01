14c6c5e266144ba830611cd9d72a311136dd7af3
usherman3741 <usherman3741@gmail.com>
2024-11-01 10:41:33 +0900

    Initial Commit

---
+++ honda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  honda
+//
+//  Created by Jung-mo on 11/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ honda/hondaApp.swift
@@ -0,0 +1,17 @@
+//
+//  hondaApp.swift
+//  honda
+//
+//  Created by Jung-mo on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct hondaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
